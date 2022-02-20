bf91ca57a375e802b339b49445123ed9910fd91d
Tung Anh Nguyen <tunganh2521999@gmail.com>
2022-02-20 23:06:04 +0700

    [update] - actions for linkedList

--- practice/dataStructure/linkedList/01-linked-list-create.js
+++ practice/dataStructure/linkedList/01-linked-list-create.js
@@ -1,6 +1,58 @@
 function createLinkedList() {
   let head = null;
 
+  function getHead() {
+    return head;
+  }
+
+  function getTail() {
+    if (head == null) return null;
+    let tail = head;
+    while (tail != null) {
+      tail = head.next;
+    }
+
+    return tail;
+  }
+
+  function getSize() {
+    if (head == null) return 0;
+
+    let count = 0;
+    let current = head;
+    while (current != null) {
+      count++;
+      current = current.next;
+    }
+
+    return count;
+  }
+
+  function findIndexByData(data) {
+    if (data == null) return -1;
+
+    let index = 0;
+    let current = head;
+    while (current != null) {
+      if (current.data === data) return index;
+      index++;
+      current = current.next;
+    }
+    return -1;
+  }
+  function findWithCallback(callback) {
+    if (!head) return undefined;
+
+    let current = head;
+    let index = 0;
+    while (current != null) {
+      if (callback(current.data, index)) return current.data;
+      index++;
+      current = current.next;
+    }
+    return undefined;
+  }
+
   function insertHead(newData) {
     const newNode = {
       data: newData,
@@ -8,18 +60,18 @@ function createLinkedList() {
     };
     if (head == null) {
       head = newNode;
-      console.log("Khi chưa có head --> gán head = newNode : ", head);
+      // console.log("Khi chưa có head --> gán head = newNode : ", head);
       return;
     }
 
     // ortherwise, linked list has some items
     newNode.next = head;
-    console.log(
-      "Khi linked list đã có item --> gán newNode.next = head : ",
-      newNode
-    );
+    // console.log(
+    //   "Khi linked list đã có item --> gán newNode.next = head : ",
+    //   newNode
+    // );
     head = newNode;
-    console.log("Khi linked list đã có item --> gán head = newNode : ", head);
+    // console.log("Khi linked list đã có item --> gán head = newNode : ", head);
   }
 
   function printList() {
@@ -32,6 +84,11 @@ function createLinkedList() {
   }
 
   return {
+    getHead,
+    getTail,
+    getSize,
+    findIndexByData,
+    findWithCallback,
     insertHead,
     printList,
   };
@@ -40,4 +97,18 @@ function createLinkedList() {
 const numberLinkedList = createLinkedList();
 numberLinkedList.insertHead(5);
 numberLinkedList.insertHead(4);
+numberLinkedList.insertHead(3);
+numberLinkedList.insertHead(2);
 numberLinkedList.printList();
+
+console.log("size: ", numberLinkedList.getSize());
+console.log("findIndexByData: ", numberLinkedList.findIndexByData(4));
+
+console.log(
+  "findWithCallback1: ",
+  numberLinkedList.findWithCallback((x) => x % 2 === 0) // 2, 4
+);
+console.log(
+  "findWithCallback2: ",
+  numberLinkedList.findWithCallback((x) => x > 5) // undefined
+);
